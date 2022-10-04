6b2c1129d55eb5ab18ddac3711d7d4f4443d8790
Akseli Hulkkonen <akseli.hulkkonen@gmail.com>
2022-10-04 22:28:15 +0300

    map doesn't work

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import L from 'leaflet';
 
-const getGeoJSON =async () =>{
+const getGeoJSON = async () => {
     const url = "https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326"
     const response = await fetch(url)
     const geoData = await response.json()
@@ -9,16 +9,44 @@ const getGeoJSON =async () =>{
 };
 
 const initMap = (geoData) => {
-    
+    console.log("initmap")
+
     let map = L.map('map', {
+        
         minZoom: -3
     })
+
     let geoJson = L.geoJSON(geoData, {
+        weight: 2,
         onEachFeature: getFeature
+         
     }).addTo(map)
 
+    console.log("initmap2")
+    let osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 13,
+        attribution: "© OpenStreetMap",
+        
+    }).addTo(map);
+
+  
+   
+    
     map.fitBounds(geoJson.getBounds())
+
+    console.log("initmap3")
 }
 
 
+const getFeature = (feature , layer ) =>{
+    console.log("initmap4")
+    if(!feature.properties.id) return;
+    const id = feature.properties.id
+    
+    console.log("testi")
+    layer.bindPopup("testing" + id)
+    
+}
+
 getGeoJSON();
+
