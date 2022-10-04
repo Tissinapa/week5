65a2a4085a3fb70f03d65a568f2f39eebf036cae
Akseli Hulkkonen <akseli.hulkkonen@gmail.com>
2022-10-04 16:31:59 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-import "./styles.css";
+import L from 'leaflet';
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const getGeoJSON =async () =>{
+    const url = "https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326"
+    const response = await fetch(url)
+    const geoData = await response.json()
+
+    initMap(geoData)
+};
+
+const initMap = (geoData) => {
+    
+    let map = L.map('map', {
+        minZoom: -3
+    })
+    let geoJson = L.geoJSON(geoData, {
+        onEachFeature: getFeature
+    }).addTo(map)
+
+    map.fitBounds(geoJson.getBounds())
+}
+
+
+getGeoJSON();
